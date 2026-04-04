3cb3461e705363951f6411d17d87298fe03621ba
kimbyeongho <skyqwn@gmail.com>
2026-04-04 22:08:42 +0900

    database SQLAlchemy 설정

---
+++ db/database.py
@@ -0,0 +1,39 @@
+from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy import QueuePool, create_engine
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DATABASE_CONN = os.getenv("DATABASE_CONN")
+print("$$$$$", DATABASE_CONN)
+
+engine = create_engine(DATABASE_CONN, #echo=True,
+                       poolclass=QueuePool,
+                       #poolclass=NullPool, # Connection Pool 사용하지 않음,
+                       pool_size=10, max_overflow=0,
+                       pool_recycle=300)
+
+def direct_get_conn():
+    conn = None
+    try:
+        conn = engine.connect()
+        return conn
+    except SQLAlchemyError as e:
+        print(e)
+        raise HTTPException(status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
+                            detail="요청하신 서비스가 잠시 내부적으로 문제가 발생하였습니다.")
+    
+def context_get_conn():
+    conn = None
+    try:
+        conn = engine.connect()
+        yield conn
+    except SQLAlchemyError as e:
+        print(e)
+        raise HTTPException(status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
+                            detail="요청하신 서비스가 잠시 내부적으로 문제가 발생하였습니다.")
+    finally:
+        if conn:
+            conn.close()
+    
